59d4752146ca797d46817ccff87fc0cda3a60b9f
andreia-n <56719935+andreia-n@users.noreply.github.com>
2021-02-01 10:05:47 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+
+const cors = require('cors');
+
+const knex = require('knex')
+
+const register = require('./controlers/register');
+const signin = require('./controlers/signin');
+const profile = require('./controlers/profile');
+const image = require('./controlers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smart_brain'
+  }
+});
+db.select('*').from('users').then(data =>{
+	console.log(data);
+});
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req, res) =>{
+
+	//res.send(database.users);
+})
+
+app.post('/signin', (req,res) =>{signin.handleSignin(req,res,db,bcrypt)})
+
+
+app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt) })
+
+
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)} )
+
+app.put('/image', (req,res) =>{image.handleImage(req,res,db)})
+
+app.post('/imageurl', (req,res) =>{image.handleApi(req,res)})
+
+
+
+app.listen(3000, () =>{
+	console.log('app is runing on port 3000');
+});
+
